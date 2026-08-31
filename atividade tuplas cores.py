fd68ea634fbfae6c8e9b8cf697ab329327ee7373
ltcmg <167623828+ltcmg@users.noreply.github.com>
2026-08-31 14:13:37 -0300

    Tuplas com Cores em Python
    Atividade desenvolvida para praticar o uso de tuplas em Python, armazenando uma lista de cores e utilizando estruturas de repetição e condições para verificar se uma cor informada pelo usuário está presente na tupla.

---
+++ atividade tuplas cores.py
@@ -0,0 +1,6 @@
+cores= ("vermelho", "azul", 'verde', 'amarelo', 'preto')
+cor=input("Digite uma cor: ")
+for cor in cores:
+    print(f"A cor escolhida foi: {cor}")
+else:
+    print("Não tem esta cor")
