39f722bddb741024421972027a672c62917da14c
ltcmg <167623828+ltcmg@users.noreply.github.com>
2026-09-03 14:40:34 -0300

    Add files via upload

---
+++ desafio lista pares e impares.py
@@ -0,0 +1,8 @@
+numeros = [12, 7, 25, 40, 18, 33, 50, 9]
+
+for numero in numeros:
+
+    if numero % 2==0:
+        print(f"O número {numero} é par")
+    else:
+        print(f"O número {numero} é ímpar")
