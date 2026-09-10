01bdaa4bf43c89fbcf4c47cb432cd81d2dedb457
ltcmg <167623828+ltcmg@users.noreply.github.com>
2026-09-10 14:13:46 -0300

    Atividade de estudo dicionário python

---
+++ dicionário  exercicio 1.py
@@ -0,0 +1,9 @@
+pessoa = {
+   "nome": "Ana",
+    "idade" : 25,
+    "cidade": "São Paulo",
+    "profissão": "Analista de Dados",
+    "Salario": 3500
+}
+for chave, valor in pessoa.items():
+    print(chave, valor)
