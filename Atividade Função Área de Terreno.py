1a1d518c3c06050e13ed862f05e51f6a4207c289
ltcmg <167623828+ltcmg@users.noreply.github.com>
2026-09-11 16:27:59 -0300

    Atividade de Python

---
+++ Atividade Função Área de Terreno.py
@@ -0,0 +1,9 @@
+def area (largura, comprimento):
+    s= largura * comprimento
+    print( f"A dimensão da área é {s}m² ")
+
+
+comprimento = int(input("Informe o comprimento da área: "))
+largura = int(input("Informe a largura da área: "))
+
+area(largura, comprimento)
