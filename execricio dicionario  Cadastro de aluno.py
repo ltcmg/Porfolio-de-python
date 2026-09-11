c6475fd445950233c9c1880e93b28303ab574662
ltcmg <167623828+ltcmg@users.noreply.github.com>
2026-09-11 14:00:13 -0300

    Atividade de Python

---
+++ execricio dicionario  Cadastro de aluno.py
@@ -0,0 +1,8 @@
+aluno={}
+  
+aluno["nome"] = input("digite o nome: "), 
+aluno["idade"] = int(input("Digite a idade: ")),
+aluno["curso"] = input("Digite o curso: "),
+aluno["nota"] = float(input("Digite a nota: "))
+
+print(aluno)
