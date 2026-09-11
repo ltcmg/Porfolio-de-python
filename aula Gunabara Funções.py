18c516d68c4631423d76032ffeb793534486d8a5
ltcmg <167623828+ltcmg@users.noreply.github.com>
2026-09-11 16:28:54 -0300

    Aula Gustavo Guanabara

---
+++ aula Gunabara Funções.py
@@ -0,0 +1,37 @@
+'''def soma (a,b):
+    print(f'A = {a} e B {b}')
+    s= a + b
+    print(f"A soma A + B = {s}")
+
+
+#programa principal
+
+soma (5,9)
+soma (56,98)'''
+
+
+'''#parametro tuplas
+def contador (*num):
+    for valor in num:
+        print(f' {valor}, end')
+    print ( "FIM!")
+
+
+contador (2, 1, 7)
+contador (8,0)
+contador (4, 4, 7, 6, 2)'''
+
+
+# parametro listas 
+'''def dobra(lst):
+    pos= 0 
+    while pos< len(lst):
+        lst[pos]*=2
+        pos +=1
+
+valores= [7,2,5,0,4]
+dobra(valores)
+print(valores)'''
+
+
+#Exercício da aula
